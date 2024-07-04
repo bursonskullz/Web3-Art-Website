a32ac64c7d6b8fcafb927033bb0a7140b409fbb0
ROY BURSON <159820187+bursonskullz@users.noreply.github.com>
2024-07-03 17:24:59 -0700

    Update contract.sol
    added functions

--- contract.sol
+++ contract.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -8,7 +9,7 @@ contract BursonSkullz is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-     // Roy's wallet address
+     // Roy's static wallet address
     address RoysWallet = 0x5CdaD7876270364242Ade65e8e84655b53398B76;
 
     // Struct to represent an NFT (Non-Fungible Token)
@@ -16,7 +17,10 @@ contract BursonSkullz is ERC721 {
         uint256 id;         // Unique identifier for the NFT
         string image;       // Image associated with the NFT (IPFS hash or URL)
         uint256 price;      // Price of the NFT in wei (1 ether = 1e18 wei)
+        uint256 decimalPlaces;
         address owner;      // Current owner of the NFT
+        uint256 mintDate;
+        string tokenName;
         bool forSale;       // Flag indicating if the NFT is for sale
     }
     // Mapping from token ID to NFT struct
@@ -26,37 +30,66 @@ contract BursonSkullz is ERC721 {
     event NFTMinted(uint256 id, address owner);
     event NFTForSale(uint256 id, uint256 price);
     event NFTSold(uint256 id, address buyer, uint256 price);
-    // Constructor to initialize the ERC721 token
-    constructor() ERC721("BursonSkullz", "Burson_Skullz") {}
+    
+    // State variables to store the owners
+    address public contractCreator;
+    string public artist;
+    address[] internal owners;
 
+    constructor() ERC721("BursonSkullz", "Burson_Skullz") {
+        contractCreator = msg.sender; // sets creator to whoever calls the constructor (or initialized the contract)
+        artist = "Roy Burson";
+        owners.push(contractCreator);
+    }
+    function checkIfOwner(address _address) internal view returns (bool) {
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (owners[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
     // Function to mint a new NFT
-    function mintNFT(address recipient, uint256 price, string memory _image) external returns (bool) {
-        // returns true if able to mint and and false otherwise so javascript can handle
-        if(msg.sender != RoysWallet){
-            // not okay to send transaction
+    function mintNFT(uint256 price, string memory _image, uint decimalPoints) public returns (bool) {
+        if(checkIfOwner(msg.sender) == false){
+            // not okay to send transaction 
+            // cannot find owner
             return false;
         }else{
             // add to array and return true
             _tokenIds.increment();  // Increment token ID counter
             uint256 newItemId = _tokenIds.current();  // Get the current token ID
-            _mint(recipient, newItemId);  // Mint the new token to the recipient
+            _mint(owners[0], newItemId);  // Mint the new token to the recipient
 
-            // Create a new NFT and store it in the mapping
+            // Create a new NFT and store it in the mapping.
             nfts[newItemId] = NFT({
                 id: newItemId,
                 image: _image,      
-                price: price,
-                owner: recipient,
+                price: price, // price in integer with decimals moved
+                decimalPlaces: decimalPoints, // real price is price/10^decimals as solidity doesnt have decimals.
+                owner: owners[0],
+                mintDate: block.timestamp,
+                tokenName: "Burson Skull",
                 forSale: true    
             });
 
             // Emit an event to signify that a new NFT has been minted
-            emit NFTMinted(newItemId, recipient);
+            emit NFTMinted(newItemId, owners[0]);
             return true;
         }
     }
 
+    function mintArrayOfNFTs(NFT[] memory nftArray) external returns (bool){
+        for(uint256 i= 0; i < nftArray.length; i++){
+            if(!mintNFT(nftArray[i].price, nftArray[i].image , nftArray[i].decimalPlaces)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     function listNFT(uint256 tokenId) external {
+        // modifiying blockchain causes gas fees so they will need to approve transaction with gas!
         require(ownerOf(tokenId) == msg.sender, "Only the owner can list the NFT");
         nfts[tokenId].forSale = true;
         emit NFTForSale(tokenId, nfts[tokenId].price);
@@ -73,7 +106,7 @@ contract BursonSkullz is ERC721 {
         uint256 tokenId = _tokenIds.current() - index - 1;
         return tokenId;
     }
-    function getAllTokens() public view returns (NFT[] memory) {
+    function getAllNFTS() public view returns (NFT[] memory) {
         uint256 totalTokens = _tokenIds.current();
         NFT[] memory tokens = new NFT[](totalTokens);
 
@@ -83,5 +116,35 @@ contract BursonSkullz is ERC721 {
         }
 
         return tokens;
+    }
+    
+    function transferSingleNFT(address recipient, uint256 tokenId) public returns (bool){
+        if(msg.sender == nfts[tokenId].owner){
+            safeTransferFrom(msg.sender, recipient, tokenId);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    function checkIfOwnerOfArray(address recipient, uint256[] tokenIdArray) internal returns (bool){
+        bool isOwnerOfallTokens;
+        for(uint256 k=0; k< tokenIdArray; k++){
+            if(msg.sender == nfts[tokenIdArray[k].owner){
+                isOwnerOfallTokens = true;
+            }else{
+                isOwnerOfallTokens = false;
+            }
+        }
+        return isOwnerOfallTokens;
+    }
+    function transferArrayOfNFTS(address recipient, uint256[] tokenIdArray) public returns (bool) {
+        if(checkIfOwnerOfArray(address recipient, uint256[] tokenIdArray)){
+            for(uint256 k=0; k< tokenIdArray; k++){
+                safeTransferFrom(msg.sender, recipient, tokenIdArray[k]);
+            }
+            return true;
+        }else {
+            return false;
+        }
     }
 }
